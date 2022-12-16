d0a6617bcd79c37372fae70376dd4790462cf597
Liutauras Adomaitis <adomaitis@kolabsystems.com>
2022-12-16 10:29:01 +0200

    comment out logger command

--- ldap-pubkeys.sh
+++ ldap-pubkeys.sh
@@ -3,10 +3,10 @@
 USER_NAME=$1
 # HOST_NAME=$(hostname)
 
-logger -p "AUTH.info" "Checking for user $1"
+# logger -p "AUTH.info" "Checking for user $1" >&2
 
 PUB_KEYS=$(ldapsearch -x -H ldap://host.containers.internal:3389/ -o ldif-wrap=no -D 'cn=Directory Manager' -w drowssap -b 'dc=test,dc=poc' 'uid=demo_user' -LLL nsSshPublicKey | grep nsSshPublicKey: | sed 's/nsSshPublicKey: //g')
 
-logger -p "AUTH.info" "Found pubkeys ${PUB_KEYS}"
+# logger -p "AUTH.info" "Found pubkeys ${PUB_KEYS}" >&2
 
 echo "${PUB_KEYS}"
